5698bf16b3402f1fac3b7cc3a2c36f74ad87b614
icepie <icepie.dev@gmail.com>
2020-07-18 20:17:35 +0800

    Improve compilation speed with the ccache compilation tool

--- build_kernel_64.sh
+++ build_kernel_64.sh
@@ -6,10 +6,10 @@ export ARCH=arm64
 export BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
-mkdir out
+mkdir out 
 chmod -R 777 out
 
 make -C $(pwd) O=$(pwd)/out -j$BUILD_JOB_NUMBER ARCH=$ARCH CROSS_COMPILE=$BUILD_CROSS_COMPILE KCFLAGS=-mno-android  dream2qlte_chn_open_defconfig
 make -C $(pwd) O=$(pwd)/out -j$BUILD_JOB_NUMBER ARCH=$ARCH CROSS_COMPILE=$BUILD_CROSS_COMPILE KCFLAGS=-mno-android
 
-cp out/arch/arm64/boot/Image* $(pwd)/arch/arm64/boot/
+#out/arch/arm64/boot/
--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:18.04
 
 RUN apt update -y > /dev/null 2> /dev/null
 
-RUN apt install python make bc git gcc -y
+RUN apt install python make bc git gcc ccache -y
 
 WORKDIR /home/kernel
 
@@ -12,5 +12,5 @@ RUN git clone  https://github.com/djb77/aarch64-linux-android-4.9 ../PLATFORM/pr
 
 COPY ./ ./
 
-CMD ["CMD"]
+CMD ["bash"]
 

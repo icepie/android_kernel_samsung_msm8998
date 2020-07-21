544971289320876f2ab92b539f40c97cca31c11c
Tea <icepie.dev@gmail.com>
2020-07-22 06:25:43 +0800

    set PLATFORM_VERSION

--- build_kernel_64.sh
+++ build_kernel_64.sh
@@ -6,6 +6,7 @@ export ARCH=arm64
 export BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 export ANDROID_MAJOR_VERSION=p
+export PLATFORM_VERSION=9.0.0
 
 mkdir out 
 chmod -R 777 out

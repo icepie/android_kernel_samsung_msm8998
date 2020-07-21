4831193ed845f82391c0ddf9a868711e2609afac
icepie <icepie.dev@gmail.com>
2020-07-21 20:25:37 +0800

    revert some commits and set android version in Makefile
    the all works for test wifi...
    hope it will be work fine...

--- build_kernel_64.sh
+++ build_kernel_64.sh
@@ -5,9 +5,6 @@ make mrproper
 export ARCH=arm64
 export BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
-export ANDROID_MAJOR_VERSION=p
-export ANDROID_VERSION=90000
-export PLATFORM_VERSION=9.0.0
 
 mkdir out 
 chmod -R 777 out

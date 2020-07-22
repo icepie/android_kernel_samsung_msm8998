8a69fe5754fc18b39f92634d38a027d094b3c233
icepie <icepie.dev@gmail.com>
2020-07-22 22:52:29 +0800

    test building android boot image

--- build_kernel_64.sh
+++ build_kernel_64.sh
@@ -13,3 +13,18 @@ chmod -R 777 out
 
 make -C $(pwd) O=$(pwd)/out -j$BUILD_JOB_NUMBER ARCH=$ARCH CROSS_COMPILE=$BUILD_CROSS_COMPILE KCFLAGS=-mno-android  dream2qlte_chn_open_defconfig
 make -C $(pwd) O=$(pwd)/out -j$BUILD_JOB_NUMBER ARCH=$ARCH CROSS_COMPILE=$BUILD_CROSS_COMPILE KCFLAGS=-mno-android
+
+# build boot.img by aik tool
+rm -rf $(pwd)/build/aik/ramdisk $(pwd)/build/aik/split_img
+cp $(pwd)/build/boot/dream2qltechn/* $(pwd)/build/aik/
+mv $(pwd)/out/arch/arm64/boot/Image-gz-dtb $(pwd)/build/aik/split_img/boot.img-zImage
+
+if [ `whoami` = "root" ];then
+    bash $(pwd)/build/aik/cleanup.sh
+    bash $(pwd)/build/aik/repackimg.sh
+else
+    sudo bash $(pwd)/build/aik/cleanup.sh
+    sudo bash $(pwd)/build/aik/repackimg.sh
+fi
+
+cp $(pwd)/build/aik/image-new.img $(pwd)/build/zip/
